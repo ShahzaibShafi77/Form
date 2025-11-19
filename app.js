765a44cac243810d9a0d59123624b42c49647600
Shahzaib Shafi <shafishahzaib15@gmail.com>
2025-11-19 15:47:27 +0500

    This form is made by me

---
+++ app.js
@@ -0,0 +1,34 @@
+
+function form() {
+   
+  let a = document.getElementById("n1").value;
+  let b = document.getElementById("n2").value;
+  let c = document.getElementById("n3").value;
+  let d = document.getElementById("n4").value;
+  let e = document.getElementById("n5").value;
+  
+  if (a =="" || b =="" || c =="" || d =="" || e =="")
+  {
+    alert("All Feild Are Mandatory !");
+    return false;
+  }
+  else if (isNaN(c))
+  {
+    alert("Only Numbers are allowed ! Please Enter Valid Number");
+    return false;
+  }
+  else if (c.length < 11 || c.length > 11)
+  {
+    alert("Only 11 Digits Are Allowed ! Please Enter Valid Number");
+    return false;
+  }
+  else if (d != e)
+  {
+    alert("Please Enter Same Password !")
+  }
+  else
+  {
+    return true;
+  }
+
+}
